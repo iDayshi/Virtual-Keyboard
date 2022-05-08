a838d82a11561be0e561886103b0c321f0236da5
Dayshi <idayshi@gmail.com>
2022-05-08 17:53:08 +0300

    feat: add base func and visual keyboard

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-let arrayKeyLowerEng = [
+localStorage.setItem('lang', 'Eng');
+let arrayKeyLowerEngArr = [
   '`',
   '1',
   '2',
@@ -64,6 +65,56 @@ let arrayKeyLowerEng = [
   'ArrowRight',
 ];
 
+let arrayKeyLowerEng = [
+  '`',
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  '0',
+  '-',
+  '=',
+  'q',
+  'w',
+  'e',
+  'r',
+  't',
+  'y',
+  'u',
+  'i',
+  'o',
+  'p',
+  '[',
+  ']',
+  '\\',
+  'a',
+  's',
+  'd',
+  'f',
+  'g',
+  'h',
+  'j',
+  'k',
+  'l',
+  ';',
+  "'",
+  'z',
+  'x',
+  'c',
+  'v',
+  'b',
+  'n',
+  'm',
+  ',',
+  '.',
+  '/',
+];
+
 let arrayKeyUpperEng = [
   '~',
   '!',
@@ -78,8 +129,6 @@ let arrayKeyUpperEng = [
   ')',
   '_',
   '+',
-  'Backspace',
-  'Tab',
   'Q',
   'W',
   'E',
@@ -93,7 +142,6 @@ let arrayKeyUpperEng = [
   '{',
   '}',
   '|',
-  'CapsLock',
   'A',
   'S',
   'D',
@@ -105,8 +153,6 @@ let arrayKeyUpperEng = [
   'L',
   ':',
   '"',
-  'Enter',
-  'ShiftLeft',
   'Z',
   'X',
   'C',
@@ -117,41 +163,134 @@ let arrayKeyUpperEng = [
   '<',
   '>',
   '?',
-  'ShiftRight',
-  'ControlLeft',
-  'Meta',
-  'AltLeft',
-  'Space',
-  'AltRight',
-  'ControlRight',
-  'ArrowLeft',
-  'ArrowUp',
-  'ArrowDown',
-  'ArrowRight',
+];
+
+let arrayKeyLowerRu = [
+  'ё',
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  '0',
+  '-',
+  '=',
+  'й',
+  'ц',
+  'у',
+  'к',
+  'е',
+  'н',
+  'г',
+  'ш',
+  'щ',
+  'з',
+  'х',
+  'ъ',
+  '\\',
+  'ф',
+  'ы',
+  'в',
+  'а',
+  'п',
+  'р',
+  'о',
+  'л',
+  'д',
+  'ж',
+  'э',
+  'я',
+  'ч',
+  'с',
+  'м',
+  'и',
+  'т',
+  'ь',
+  'б',
+  'ю',
+  '.',
+];
+
+let arrayKeyUpperRu = [
+  'Ё',
+  '!',
+  '@',
+  '#',
+  '$',
+  '%',
+  '^',
+  '&',
+  '*',
+  '(',
+  ')',
+  '_',
+  '+',
+  'Й',
+  'Ц',
+  'У',
+  'К',
+  'Е',
+  'Н',
+  'Г',
+  'Ш',
+  'Щ',
+  'З',
+  'Х',
+  'Ъ',
+  '/',
+  'Ф',
+  'Ы',
+  'В',
+  'А',
+  'П',
+  'Р',
+  'О',
+  'Л',
+  'Д',
+  'Ж',
+  'Э',
+  'Я',
+  'Ч',
+  'С',
+  'М',
+  'И',
+  'Т',
+  'Ь',
+  'Б',
+  'Ю',
+  ',',
 ];
 
 const button = document.querySelector('.centr');
 const blockKeyBoard = document.createElement('div');
-const arrowBlock = document.createElement('div');
 blockKeyBoard.classList.add('position');
 document.body.append(blockKeyBoard);
 
 const keyLowerEng = (keys) => {
+  const arrowBlock = document.createElement('div');
   keys.map((key) => {
     const keyBlock = document.createElement('div');
     keyBlock.id = key;
-    keyBlock.innerText = `${key}`;
+    keyBlock.innerText = key;
     if (key === 'Backspace') {
       keyBlock.classList.add('key-backspace', 'key-other');
+      keyBlock.innerText = `Backspace`;
     }
     if (key === 'Tab') {
       keyBlock.classList.add('key-tab', 'key-other');
+      keyBlock.innerText = `Tab`;
     }
     if (key === 'CapsLock') {
       keyBlock.classList.add('key-capslock', 'key-other');
+      keyBlock.innerText = `Caps`;
     }
     if (key === 'Enter') {
       keyBlock.classList.add('key-enter', 'key-other');
+      keyBlock.innerText = `Enter`;
     }
     if (key === 'ShiftLeft' || key === 'ShiftRight') {
       if (key === 'ShiftRight') {
@@ -176,6 +315,7 @@ const keyLowerEng = (keys) => {
     }
     if (key === 'Space') {
       keyBlock.classList.add('key-space', 'key-other');
+      keyBlock.innerText = `Space`;
     }
     if (key === 'ArrowLeft' || key === 'ArrowRight') {
       keyBlock.classList.add('key-other');
@@ -183,23 +323,22 @@ const keyLowerEng = (keys) => {
     }
     if (key === 'ArrowUp') {
       keyBlock.classList.add('key-arrow-up', 'key-other');
-      keyBlock.innerText = `Bok`;
+      keyBlock.innerText = `Up`;
       arrowBlock.append(keyBlock);
+      return;
     }
-    console.log(arrowBlock);
+
     if (key === 'ArrowDown') {
       keyBlock.classList.add('key-arrow-down', 'key-other');
-      keyBlock.innerText = `Bok`;
+      keyBlock.innerText = `Down`;
       arrowBlock.append(keyBlock);
       blockKeyBoard.append(arrowBlock);
+      return;
     }
-    console.log(arrowBlock);
+
     if (key.length === 1) {
       keyBlock.classList.add('key-default');
     }
-    // key.addEventListener('click', (event) => {
-    //   button.classList.add('active');
-    // });
     blockKeyBoard.append(keyBlock);
   });
 };
@@ -210,10 +349,41 @@ document.addEventListener('keydown', (event) => {
   console.log(event.code);
   const buttonOther = document.getElementById(event.code);
   const button = document.getElementById(event.key);
-  if (event.code === 'ShiftLeft' || event.code === 'ShiftRight') {
-    blockKeyBoard
-      .querySelectorAll('.key-default')
-      .forEach((key, index) => (key.textContent = arrayKeyUpperEng[index]));
+  if (event.ctrlKey === true && event.altKey === true) {
+    if (localStorage.getItem('lang') === 'Eng') {
+      localStorage.setItem('lang', 'Ru');
+      return blockKeyBoard
+        .querySelectorAll('.key-default')
+        .forEach((key, index) => {
+          key.textContent = arrayKeyLowerRu[index];
+        });
+    } else {
+      if (localStorage.getItem('lang') === 'Ru') {
+        localStorage.setItem('lang', 'Eng');
+        return blockKeyBoard
+          .querySelectorAll('.key-default')
+          .forEach((key, index) => {
+            key.textContent = arrayKeyLowerEng[index];
+          });
+      }
+    }
+  }
+  if (
+    (localStorage.getItem('lang') === 'Ru' && event.code === 'ShiftLeft') ||
+    event.code === 'ShiftRight'
+  ) {
+    blockKeyBoard.querySelectorAll('.key-default').forEach((key, index) => {
+      key.textContent = arrayKeyUpperRu[index];
+    });
+    buttonOther.classList.add('active');
+  }
+  if (
+    (localStorage.getItem('lang') === 'Eng' && event.code === 'ShiftLeft') ||
+    event.code === 'ShiftRight'
+  ) {
+    blockKeyBoard.querySelectorAll('.key-default').forEach((key, index) => {
+      key.textContent = arrayKeyUpperEng[index];
+    });
     buttonOther.classList.add('active');
   } else if (
     event.code === 'AltLeft' ||
@@ -228,10 +398,22 @@ document.addEventListener('keydown', (event) => {
   }
 
   document.addEventListener('keyup', (event) => {
-    if (event.code === 'ShiftLeft' || event.code === 'ShiftRight') {
+    if (
+      (localStorage.getItem('lang') === 'Eng' && event.code === 'ShiftLeft') ||
+      event.code === 'ShiftRight'
+    ) {
       blockKeyBoard
         .querySelectorAll('.key-default')
         .forEach((key, index) => (key.textContent = arrayKeyLowerEng[index]));
+      buttonOther.classList.remove('active');
+    }
+    if (
+      (localStorage.getItem('lang') === 'Ru' && event.code === 'ShiftLeft') ||
+      event.code === 'ShiftRight'
+    ) {
+      blockKeyBoard
+        .querySelectorAll('.key-default')
+        .forEach((key, index) => (key.textContent = arrayKeyLowerRu[index]));
       buttonOther.classList.remove('active');
     }
     if (buttonOther) {
@@ -242,4 +424,14 @@ document.addEventListener('keydown', (event) => {
   });
 });
 
-keyLowerEng(arrayKeyLowerEng);
+// document.addEventListener('click', (event) => {
+//   console.log(event.target);
+//   console.log(event.code);
+//   const buttonOther = document.getElementById(event.code);
+//   const button = document.getElementById(event.key);
+//   if (event.target) {
+//     event.target.classList.add('active');
+//   }
+// });
+
+keyLowerEng(arrayKeyLowerEngArr);
